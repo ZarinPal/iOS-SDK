2ec1ee9865fb6107884a676e6e65833ae1e2528c
farshidroohi <farshid.roohi.a@gmail.com>
2021-06-02 12:02:34 +0430

    Create ZarinPal Request

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import ZarinPal
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,PaymentCallback {
+    
+    #warning("Please get your merchant id from ZarinPal panel(https://next.zarinpal.com/panel/dashboard)")
+    let merchantID = "your merchant id"
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func btnPay(_ sender: Any) {
+        
+        
+        let request = Request.asPaymentRequest(merchantID: self.merchantID, amount: 1000, callbackURL: "https://www.google.com", description: "your descaription for payment")
+        
+        let zarinPal = ZarinPal()
+        zarinPal.start(request: request, callback: self,vc: self)
+    }
+    
+    func didClose() {
+        print("did close")
+    }
+    
+    func didException(exception: NSException) {
+        print("exception : \(exception.description)")
+    }
+    
+    func didReceive(receipt: ReceiptShaparak, raw: String) {
+        print("receipt : \(receipt)")
+    }
+    
 }
 

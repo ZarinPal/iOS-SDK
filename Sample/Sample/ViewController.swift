ecb683d5c5e2943609ed9530d3f60ee6116b162f
farshidroohi <farshid.roohi.a@gmail.com>
2021-06-02 11:51:33 +0430

    init sample project

---
+++ Sample/Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by ZarinPal on 6/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
